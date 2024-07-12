504c35f42820e865606558de0b30fea20d86f78e
Dowland Aiello <dowlandaiello@gmail.com>
2024-07-12 16:33:52 -0700

    Separate testing utils into a separate flake.

--- local-interchaintest/flake.nix
+++ local-interchaintest/flake.nix
@@ -8,37 +8,42 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: rec {
-      local-ic = let
-        repo = nixpkgs.fetchFromGitHub {
-          owner = "strangelove-ventures";
-          repo = "interchaintest";
-          rev = "v8.5.0";
-          hash = "sha256-NKp0CFPA593UNG/GzMQh7W/poz1/dESrqlRG8VQVxUk=";
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
         };
-      in nixpkgs.buildGoModule rec {
-        pname = "local-ic";
-        version = "8.5.0";
-        src = repo;
-        proxyVendor = true;
-        subPackages = [ "local-interchain/cmd/local-ic" ];
-        vendorHash = "sha256-NWq2/gLMYZ7T5Q8niqFRJRrfnkb0CjipwPQa4g3nCac=";
-      };
-      local-interchaintest = nixpkgs.rustPlatform.buildRustPackage {
-        name = "local-interchaintest";
-        src = ./local-interchaintest;
-        nativeBuildInputs = nativeBuildInputs;
-        buildInputs = buildInputs ++ [ local-ic ];
-        cargoSha256 = nixpkgs.lib.fakeHash;
-        cargoLock = {
-          lockFile = ./local-interchaintest/Cargo.lock;
-          outputHashes = {
-            "localic-std-0.0.1" =
-              "sha256-v2+BGy7aH63B5jR8/oR0CSHOUBgNdfk+8JgNKfOFaq0=";
-            "localic-utils-0.1.0" =
-              "sha256-1Xg2XSJXqWfCJ4MB6ElrsVYpztXSzAl7HFAZ12QRhfo=";
+      in rec {
+        packages.local-ic = let
+          repo = pkgs.fetchFromGitHub {
+            owner = "strangelove-ventures";
+            repo = "interchaintest";
+            rev = "v8.5.0";
+            hash = "sha256-NKp0CFPA593UNG/GzMQh7W/poz1/dESrqlRG8VQVxUk=";
+          };
+        in pkgs.buildGoModule rec {
+          pname = "local-ic";
+          version = "8.5.0";
+          src = repo;
+          proxyVendor = true;
+          subPackages = [ "local-interchain/cmd/local-ic" ];
+          vendorHash = "sha256-NWq2/gLMYZ7T5Q8niqFRJRrfnkb0CjipwPQa4g3nCac=";
+        };
+        packages.local-interchaintest = pkgs.rustPlatform.buildRustPackage {
+          name = "local-interchaintest";
+          src = ./.;
+          nativeBuildInputs = [ ];
+          buildInputs = [ packages.local-ic ];
+          cargoSha256 = nixpkgs.lib.fakeHash;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "localic-std-0.0.1" =
+                "sha256-v2+BGy7aH63B5jR8/oR0CSHOUBgNdfk+8JgNKfOFaq0=";
+              "localic-utils-0.1.0" =
+                "sha256-1Xg2XSJXqWfCJ4MB6ElrsVYpztXSzAl7HFAZ12QRhfo=";
+            };
           };
         };
-      };
-    });
+      });
 }
