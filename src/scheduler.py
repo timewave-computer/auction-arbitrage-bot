95a5eae3f5e02eb329f8a09741c01ba1b9a5eaff
Dowland Aiello <dowlandaiello@gmail.com>
2024-10-03 21:06:16 +0000

    Make log balance prefix lowercase.

--- src/scheduler.py
+++ src/scheduler.py
@@ -153,7 +153,7 @@ class Ctx(Generic[TState]):
         )
 
         if balance_resp_in:
-            prefix += f"BALANCE[{route.legs[0].in_asset()[:DENOM_BALANCE_PREFIX_MAX_DENOM_LEN]}]: {balance_resp_in} "
+            prefix += f"balance[{route.legs[0].in_asset()[:DENOM_BALANCE_PREFIX_MAX_DENOM_LEN]}]: {balance_resp_in} "
 
         balance_resp_base_denom = try_multiple_clients(
             self.clients[route.legs[0].backend.chain_id],
@@ -166,7 +166,7 @@ class Ctx(Generic[TState]):
         )
 
         if balance_resp_base_denom:
-            prefix += f"BALANCE[{self.cli_args['base_denom'][:DENOM_BALANCE_PREFIX_MAX_DENOM_LEN]}]: {balance_resp_in} "
+            prefix += f"balance[{self.cli_args['base_denom'][:DENOM_BALANCE_PREFIX_MAX_DENOM_LEN]}]: {balance_resp_in} "
 
         route.logs.append(f"{log_level.upper()} {prefix}{fmt_string % tuple(args)}")
 
